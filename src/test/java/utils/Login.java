ddcc9254bc0f87b754c08bb5ba1f352eaf31435e
HariSivaraj <harivignes@hotmail.com>
2025-11-28 14:19:38 +0530

    git soft reset example

--- src/test/java/utils/Login.java
+++ src/test/java/utils/Login.java
@@ -4,12 +4,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
 public class Login {
 
 	@Test
 	public void loginApp() {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://google.com");
+		
+		System.out.println("this is example for git");
 	}
 
 }
